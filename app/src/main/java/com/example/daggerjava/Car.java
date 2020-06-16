c53d43045cae7e807fa89efbc8c0738ba979eaa2
fateme <f_maleki@hotmail.com>
2020-06-16 12:39:22 +0430

    Method Injection

--- app/src/main/java/com/example/daggerjava/Car.java
+++ app/src/main/java/com/example/daggerjava/Car.java
@@ -6,13 +6,15 @@ import javax.inject.Inject;
 
 public class Car {
     private static final String TAG = "Car";
-    @Inject Engine engine;
+    @Inject
+    Engine engine;
 
     @Inject
     public Car() {
         Log.d(TAG, "car created");
     }
 
+    @Inject
     public void drive() {
         Log.d(TAG, "driving");
     }
